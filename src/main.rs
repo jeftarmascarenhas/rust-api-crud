78d310e80b3137f3a9027e5d67d7ac9735e85eef
jeff mascarenhas <>
2022-10-10 22:57:38 -0300

    feat: implmenting api with rocket framework

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Init crud");
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+mod routes;
+mod services;
+pub mod entities;
+
+use routes::user::*;
+
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/api/v1", routes![index, hello])
 }
