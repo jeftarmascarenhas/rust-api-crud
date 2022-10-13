a377102acf8954fa71bddad81ac139cc916b85a0
jeff mascarenhas <jeftarmascarenhas@hotmail.com>
2022-10-13 19:25:09 -0300

    test: hello response testing

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,30 @@ mod repository;
 pub mod models;
 
 use api::user::*;
+use rocket::{Rocket, Build};
 
 
 #[launch]
-fn rocket() -> _ {
+fn rocket() -> Rocket<Build> {
     rocket::build().mount("/api/v1", routes![index, hello])
+}
+
+#[cfg(test)]
+mod tests {
+  use super::rocket;
+  use rocket::local::blocking::Client;
+  use rocket::http::Status;
+
+  #[test] 
+  fn hello() {
+    let name = "Jeff";
+    let age = 36;
+    let client = Client::tracked(rocket()).expect("valid rocket instance");
+    let response = client.get(uri!("/api/v1", super::hello(name, age))).dispatch();
+
+    let result = format!("Hello, {} years old named {}!", age, name);
+
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.into_string().unwrap(), result);
+  }
 }
